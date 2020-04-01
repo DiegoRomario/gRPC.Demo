3e0e96bd716343e85a921ceea739832fab3fd354
Diego Romario <diego.romario@outlook.com>
2020-04-01 11:57:16 -0300

    adding new features

--- src/gRPCDemoClient/Program.cs
+++ src/gRPCDemoClient/Program.cs
@@ -7,16 +7,31 @@ namespace gRPCDemoClient
 {
     class Program
     {
-        static async Task Main(string[] args)   
+        static async Task Main(string[] args)
         {
             System.Threading.Thread.Sleep(TimeSpan.FromSeconds(2));
             var channel = GrpcChannel.ForAddress("https://localhost:5001");
             var client = new gRPCDemoHost.Greeter.GreeterClient(channel);
-            var response = client.SayHello(new gRPCDemoHost.HelloRequest() { Name = "Demo Client!" });
-            Console.WriteLine(response.Message);
-            await channel.ShutdownAsync();
-            Console.WriteLine("Press any key to exit...");
-            Console.ReadKey();
+            Console.WriteLine("Which Stock would you like to subscribe to follow the quote?");
+            string stock = Console.ReadLine();
+            Console.WriteLine("Please select the mode: [A]utomatic or [M]anual");
+            string mode = Console.ReadLine();
+            while (true)
+            {
+                var response = client.SayHello(new gRPCDemoHost.HelloRequest() { Name = stock });
+                Console.WriteLine(response.Message);
+                await channel.ShutdownAsync();
+                if (mode == "M")
+                {
+                    Console.WriteLine("Press any key to see the current quote...");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.WriteLine("Loading...");
+                    System.Threading.Thread.Sleep(TimeSpan.FromSeconds(1));
+                }
+            }
         }
     }
 }
--- src/gRPCDemoHost/Services/GreeterService.cs
+++ src/gRPCDemoHost/Services/GreeterService.cs
@@ -17,10 +17,24 @@ namespace gRPCDemoHost
 
         public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
         {
+            Random random = new Random();
+            double value = random.NextDouble(5.00, 5.50);
             return Task.FromResult(new HelloReply
             {
-                Message = $"Hello {request.Name} ;)"
+                Message = $"Stock: {request.Name.ToUpper()}:\nPrice: $ {value.ToString("f2")}"
             });
+        }
+        
+    }
+
+    public static class RandomExtensions
+    {
+        public static double NextDouble(
+            this Random random,
+            double minValue,
+            double maxValue)
+        {
+            return random.NextDouble() * (maxValue - minValue) + minValue;
         }
     }
 }
